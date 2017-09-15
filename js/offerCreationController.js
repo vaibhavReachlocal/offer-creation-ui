531dd9f86bf9284bcde9380a8c6c8e59d567e280
vaibhavReachlocal <vaibhav.shah@reachlocal.com>
2017-09-15 12:11:28 +0530

    Adding Styling

--- js/offerCreationController.js
+++ js/offerCreationController.js
@@ -1,8 +1,31 @@
 app.controller('offerCreationController', function ($scope,offerService) {
     $scope.cities = "hi";
+    $scope.offerDetails = {
+    		"offer":{
+    			"Name":"Name of offer",
+    			"Des 1":"Des 1"
+    		},
+    		"tab1":[{
+    			"Col1":"col1",
+    			"col2" :"col2"
+    		},{
+    			"Col1":"col1",
+    			"col2" :"col2"
+    		}],
+    		"tab2":[{
+    			"Col1":"col1",
+    			"col2" :"col2"
+    		},{
+    			"Col1":"col1",
+    			"col2" :"col2"
+    		}]
+    
+    }
     $scope.fetchDetails = function(){
     offerService.fetchOffer($scope.id).then(function(response){
 		$scope.offerDetails = response; 
+		
+		
 		console.log(response);
 	});
     }
